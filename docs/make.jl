b1f9a83eabb2914fde7284afd35bef56afd22d70
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-04-28 13:35:29 +0200

    fmt

--- docs/make.jl
+++ docs/make.jl
@@ -11,22 +11,16 @@ end
 DocMeta.setdocmeta!(TrixiAero, :DocTestSetup, :(using TrixiAero); recursive = true)
 
 # Make documentation
-makedocs(
-    modules = [TrixiAero],
-    sitename = "TrixiAero.jl",
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true",
-        canonical = "https://acom-rwth.github.io/TrixiAero.jl/stable"
-    ),
-    pages = [
-        "Home" => "index.md",
-        "API Reference" => "api.md",
-    ]
-)
+makedocs(modules = [TrixiAero],
+         sitename = "TrixiAero.jl",
+         format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true",
+                                  canonical = "https://acom-rwth.github.io/TrixiAero.jl/stable"),
+         pages = [
+             "Home" => "index.md",
+             "API Reference" => "api.md"
+         ])
 
 # Deploy documentation
-deploydocs(
-    repo = "github.com/ACoM-RWTH/TrixiAero.jl.git",
-    devbranch = "main",
-    push_preview = true
-)
+deploydocs(repo = "github.com/ACoM-RWTH/TrixiAero.jl.git",
+           devbranch = "main",
+           push_preview = true)
